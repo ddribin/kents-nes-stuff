d36527e5d8881e19c07ab56f28a4985ae3d69114
Kent Hansen <kentmhan@gmail.com>
2009-12-19 03:08:51 +0100

    add Legend of the Black Shawarma medley

---
+++ src/music/shawarma/shawarma.asm
@@ -0,0 +1,65 @@
+.codeseg
+
+.public shawarma_song
+
+shawarma_instrument_table:
+.dw env1 : .db $00,$00,$00,$92 : .db $00,$00 ; 0
+.dw env2 : .db $00,$00,$00,$22 : .db $00,$00 ; 1
+.dw env0 : .db $00,$00,$00,$42 : .db $00,$00 ; 2
+.dw env7 : .db $00,$00,$00,$00 : .db $00,$00 ; 3 triangle (short)
+.dw env0 : .db $00,$00,$00,$00 : .db $00,$00 ; 4 triangle (infinite)
+.dw env3 : .db $00,$00,$00,$00 : .db $00,$00 ; 5 noise (closed)
+.dw env4 : .db $00,$00,$00,$00 : .db $00,$00 ; 6 noise (open)
+.dw env5 : .db $00,$00,$00,$00 : .db $00,$00 ; 7 noise (snare)
+.dw env6 : .db $00,$00,$00,$00 : .db $00,$00 ; 8 noise (cymbalo)
+.dw env8 : .db $00,$00,$00,$80 : .db $00,$00 ; 9 noise (weird)
+.dw env9 : .db $00,$00,$00,$22 : .db $00,$00 ; 10 square (short)
+
+env0:
+.db $08
+.db $00,$00,$FF,$08
+.db $08,$00,$00,$00
+.db $FF,$FF
+env1:
+.db $08
+.db $00,$00,$FF,$08
+.db $08,$00,$00,$00
+.db $FF,$FF
+env2:
+.db $08
+.db $00,$00,$FF,$08
+.db $08,$00,$00,$00
+.db $FF,$FF
+env3:
+.db $08
+.db $01,$80,$00,$00
+.db $FF,$FF
+env4:
+.db $09
+.db $00,$90,$00,$00
+.db $FF,$FF
+env5:
+.db $08
+.db $00,$70,$00,$00
+.db $FF,$FF
+env6:
+.db $0A
+.db $00,$40,$00,$00
+.db $FF,$FF
+env7:
+.db $06
+.db $01,$00,$00,$00
+.db $FF,$FF
+env8:
+.db $04
+.db $01,$00,$00,$00
+.db $FF,$FF
+env9:
+.db $08
+.db $01,$80,$FF,$02
+.db $02,$00,$00,$00
+.db $FF,$FF
+
+.include "shawarma.inc"
+
+.end
---
+++ src/music/shawarma/songtable.asm
@@ -0,0 +1,11 @@
+.codeseg
+
+.public song_table
+
+.extrn shawarma_song:label
+
+song_table:
+.dw 0
+.dw shawarma_song
+
+.end
